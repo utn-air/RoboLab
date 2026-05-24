4d2856a9f4faac55afe3d78ed788c49899bdfa0b
nisarganc <nisarganc7@gmail.com>
2026-05-24 18:51:41 +0000

    fix: Update TODO list and enhance angled reach statistics script for improved data collection

--- angledreach_statistics.py
+++ angledreach_statistics.py
@@ -13,7 +13,7 @@ simple_tasks = [
 for folder_path in folder_paths:
     for task in simple_tasks:
         # open the run_i.hdf5 files within folder_path/task/
-        for run_i in range(2):
+        for run_i in range(5):
             hdf5_path = Path(folder_path) / task / f"run_{run_i}.hdf5"
             with h5py.File(hdf5_path, "r") as f:
                 print(f["data"]['demo_0']['ee_pose']['position'])
